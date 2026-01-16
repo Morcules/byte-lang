7780ea0d034ebfe4fbbf469ac9d440ff3215945e
Morcules <morcules@protonmail.com>
2026-01-16 14:21:01 +0100

    added chars

--- project/output.s
+++ project/output.s
@@ -1,15 +1,6 @@
 .global _main
 .align 16
 .text
-_3:
-ldr x10, [sp, #56]
-str x10, [sp, #3]
-mov x10, #10
-str x10, [sp, #3]
-mov x0, #0
-bl _term
-b _main_1
-
 _term:
 str x30, [sp, #-16]!
 sub sp, sp, #16
@@ -19,6 +10,67 @@ mov x1, #12
 mov x16, #1
 svc #0x80
 add sp, sp, #16
+ldr x30, [sp], #16
+ret
+
+_3:
+ldr x10, [sp, #56]
+str x10, [sp, #2]
+mov x10, #10
+str x10, [sp, #2]
+mov x0, #0
+bl _term
+b _main_1
+
+_4:
+mov x0, #1
+bl _term
+b _main_1
+
+_main:
+str x30, [sp, #-16]!
+sub sp, sp, #64
+mov x10, #5
+str x10, [sp, #56]
+mov w10, #2
+strh w10, [sp, #54]
+mov w10, #1
+strb w10, [sp, #53]
+mov w10, #97
+strb w10, [sp, #52]
+mov x10, #30
+str x10, [sp, #44]
+ldr x10, [sp, #44]
+str x10, [sp, #36]
+mov x10, #5
+str x10, [sp, #28]
+mov x10, #10
+str x10, [sp, #20]
+mov w10, #30
+strb w10, [sp, #19]
+ldrb w10, [sp, #19]
+strb w10, [sp, #18]
+mov x10, #0
+str x10, [sp, #10]
+ldr x10, [sp, #20]
+ldr x11, [sp, #28]
+cmp x10, x11
+b.ne _3
+b.eq _4
+b _main_1
+_main_1:
+mov x10, #1
+str x10, [sp, #10]
+sub sp, sp, #16
+ldr x10, [sp, #72]
+str x10, [sp, #8]
+ldr x10, [sp, #52]
+str x10, [sp, #0]
+bl _test
+add sp, sp, #16
+ldr x0, [sp, #10]
+bl _term
+add sp, sp, #64
 ldr x30, [sp], #16
 ret
 
@@ -32,54 +84,4 @@ str x10, [sp, #8]
 add sp, sp, #16
 ldr x30, [sp], #16
 ret
-
-_main:
-str x30, [sp, #-16]!
-sub sp, sp, #64
-mov x10, #5
-str x10, [sp, #56]
-mov w10, #2
-strh w10, [sp, #54]
-mov w10, #1
-strb w10, [sp, #53]
-mov x10, #30
-str x10, [sp, #45]
-ldr x10, [sp, #45]
-str x10, [sp, #37]
-mov x10, #5
-str x10, [sp, #29]
-mov x10, #10
-str x10, [sp, #21]
-mov w10, #30
-strb w10, [sp, #20]
-ldrb w10, [sp, #20]
-strb w10, [sp, #19]
-mov x10, #0
-str x10, [sp, #11]
-ldr x10, [sp, #21]
-ldr x11, [sp, #29]
-cmp x10, x11
-b.ne _3
-b.eq _4
-b _main_1
-_main_1:
-mov x10, #1
-str x10, [sp, #11]
-sub sp, sp, #16
-ldr x10, [sp, #72]
-str x10, [sp, #8]
-ldr x10, [sp, #53]
-str x10, [sp, #0]
-bl _test
-add sp, sp, #16
-ldr x0, [sp, #11]
-bl _term
-add sp, sp, #64
-ldr x30, [sp], #16
-ret
-
-_4:
-mov x0, #1
-bl _term
-b _main_1
 
